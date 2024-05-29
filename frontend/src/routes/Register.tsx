9dcb7ef2696b387f7706d3418d27ad694aff7cac
Christopher Tan <phertan02@gmail.com>
2024-05-29 14:18:42 +0800

    basic routes

---
+++ frontend/src/routes/Register.tsx
@@ -0,0 +1,16 @@
+import HomeNavbar from "../components/HomeNavbar";
+import RegisterForm from "../components/RegisterForm";
+
+
+function Register() {
+
+
+  return (
+    <>
+      {/* <HomeNavbar/> */}
+      <RegisterForm/>
+    </>
+  )
+}
+
+export default Register;
---
+++ frontend/src/routes/Root.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeNavbar from '../components/HomeNavbar';
+import Testing from '../components/Testing';
+
+function Root() {
+  return <>
+    <HomeNavbar/>
+    <Testing/>
+  </>
+}
+
+export default Root;
