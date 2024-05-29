798b9e1423f3a71c715f4c46ced3483dc4a05215
Christopher Tan <phertan02@gmail.com>
2024-05-29 14:15:28 +0800

    add models for cards

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column 
-from sqlalchemy import String, Integer, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column , relationship
+from sqlalchemy import String, Integer, Text, ForeignKey
 from .extensions import db
 
 class User(db.Model) :
@@ -10,3 +10,16 @@ class User(db.Model) :
     password: Mapped[str] = mapped_column(Text, nullable=False)
     email: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
 
+class Card(db.Model):
+    __tablename__ = "cards"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    header: Mapped[str] = mapped_column(Text, nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=True)
+    header_flipped: Mapped[str] = mapped_column(Text, nullable=False)
+    body_flipped: Mapped[str] = mapped_column(Text, nullable=True)
+    # deck_id: Mapped[int] = 
+    
+
+class Deck(db.Model):
+    __tablename__ = "decks"
+    id: Mapped[int] = mapped_column(primary_key=True)
