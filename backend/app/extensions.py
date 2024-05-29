d63755018c3d690a22096026393a5d29826f23df
Christopher Tan <phertan02@gmail.com>
2024-05-29 14:36:54 +0800

    add default sqlalchemy base model

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,11 +1,16 @@
 # move all extension objects here
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
-db = SQLAlchemy()
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 jwt_manager = JWTManager()
 
