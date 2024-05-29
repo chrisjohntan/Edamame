adeec70d8cd51ae7973df5cc1fc6c0674bc9609f
Christopher Tan <phertan02@gmail.com>
2024-05-29 14:37:18 +0800

    minor changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,16 +1,16 @@
-from flask import Flask # quart maybe?
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+# from flask import Flask # quart maybe?
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db" # change to postgresql
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db" # change to postgresql
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
-class Config: 
-    SQLALCHEMY_DATABASE_URI = ""
+# class Config: 
+#     SQLALCHEMY_DATABASE_URI = ""
 
