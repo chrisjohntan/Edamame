1aeac0a79dbbb51c593ad116937844e1474ddbcd
Christopher Tan <phertan02@gmail.com>
2024-05-30 23:59:40 +0800

    modify to allow credentials

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // TODO: change to env variable
 const instance = axios.create({baseURL:"http://localhost:8080"})
+instance.defaults.withCredentials=true;
 
 // axios.defaults.baseURL = "http://localhost:8080"
 
