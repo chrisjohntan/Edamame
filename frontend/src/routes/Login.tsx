10f1633ef67d9e31cdef80b73b9ef8d29e69654e
Christopher Tan <phertan02@gmail.com>
2024-05-31 01:23:10 +0800

    do not loop redirect

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,9 +1,11 @@
+import { Navigate } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
     <>
       <LoginForm/>
+      {/* <Navigate to={"/dashboard"} /> do not do this, infinite loop */}
     </>
   )
 }
