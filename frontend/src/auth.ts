c90780a3ab9c39e8475b7f1aa86b8febcc4a966f
Christopher Tan <phertan02@gmail.com>
2024-05-31 01:19:14 +0800

    added login and signout functions

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,5 +1,6 @@
 import { default as axios } from "./axiosConfig"
 import type { User } from "./types";
+import { redirect } from "react-router-dom";
 
 // check if user is still logged in
 export const getCurrentUser: ()=> Promise<User|undefined> = async () => {
@@ -18,4 +19,22 @@ export function getCookie(name: string) {
   const value = `; ${document.cookie}`;
   const parts: string[] | undefined = value.split(`; ${name}=`);
   if (parts.length === 2) return parts?.pop()?.split(';').shift();
+}
+
+export const signOut = async () => {
+  try {
+    await axios.post('/logout', {}, { withCredentials: true });
+    return redirect('/login'); // Redirect to login page after logout
+  } catch (error) {
+    console.error('Error during sign out', error);
+  }
+};
+
+export const logIn = async (data: {username: string; password: string}) => {
+  try {
+    await axios.post('login', data);
+    // navigate('/dashboard');
+  } catch (error) {
+    console.error("Error during login", error);
+  }
 }
