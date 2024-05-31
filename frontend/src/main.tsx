11a27fc0047e0f063d6269db5205f89377e0a3e5
Christopher Tan <phertan02@gmail.com>
2024-06-01 00:34:53 +0800

    Refactor router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,38 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './routes/Root.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage.tsx'
 import Register from './routes/Register.tsx'
 import Login from './routes/Login.tsx'
 import Dashboard from './routes/Dashboard.tsx';
+import ProtectedRoute from './routes/ProtectedRoute.tsx';
+import { AuthProvider } from './context/AuthProvider.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import ProtectedRoute from './routes/ProtectedRoute.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/signup",
-    element: <Register />
-  },
-  {
-    path: "/login",
-    element: <Login/>
-  },
-  {
-    path: "/dashboard",
-    element: <ProtectedRoute/>,
-    children: [{path: "", element: <Dashboard/>}] 
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
+    <Route path="*"><ErrorPage/></Route>
+    </BrowserRouter>
   </React.StrictMode>
 )
