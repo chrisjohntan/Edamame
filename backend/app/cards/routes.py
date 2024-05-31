093ee6798cb43441c97d6503b6b7d7b38b11c7cf
Jia Hong <jhkoh0416@gmail.com>
2024-05-31 11:26:44 +0800

    add create_card function

--- backend/app/cards/routes.py
+++ backend/app/cards/routes.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request, jsonify, abort
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required
 import httpx
+from ..models import Card
 
 
 
@@ -12,7 +13,28 @@ cards = Blueprint("cards", __name__)
 @cards.route("/create_card", methods=["POST"])
 @jwt_required
 def create_card():
-    pass
+    header = request.json["header"]
+    body = request.json["body"]
+    header_flipped = request.json["header_flipped"]
+    body_flipped = request.json["body_flipped"]
+
+    card = Card(
+        header=header,
+        body=body,
+        header_flipped=header_flipped,
+        body_flipped=body_flipped
+    )
+
+    db.session.add(card)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Card created",
+        "card": {
+            "header": header, "body": body, "header_flipped": header_flipped, "body_flipped": body_flipped
+        }
+    }), HTTPStatus.CREATED
+
 
 @cards.route("/card", methods=["GET"])
 @jwt_required
