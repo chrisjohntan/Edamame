bf2c82bcbb0f0bcf35ba83797cc07c89ec6f9c88
Christopher Tan <phertan02@gmail.com>
2024-06-03 02:04:31 +0800

    Include card group component

--- frontend/src/routes/Dashboard.tsx
+++ frontend/src/routes/Dashboard.tsx
@@ -1,13 +1,16 @@
 import { signOut } from "../auth";
+import CardGroupDemo from "../components/CardGroupDemo";
+import LoggedNavbar from "../components/LoggedNavbar";
 
 
 function Dashboard() {
   return (
     <>
-    <div>hi</div>
+    <LoggedNavbar></LoggedNavbar>
+    {/* <div>hi</div> */}
     {/* TODO: redirect needs work. this whole component actually */}
-    <button onClick={signOut}>Sign out</button>
-    
+    {/* <button onClick={signOut}>Sign out</button> */}
+    <CardGroupDemo></CardGroupDemo>
     </>
   )
 }
