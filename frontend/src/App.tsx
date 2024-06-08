a83578224b492dcfb1a6e3265765e55484ada11e
Christopher Tan <phertan02@gmail.com>
2024-06-08 22:30:36 +0800

    move routing to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,44 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainNavbar from './components/HomeNavbar';
-import Testing from './components/Testing';
+import Root from './routes/Root.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage.tsx'
+import Register from './routes/Register.tsx'
+import Login from './routes/Login.tsx'
+import Dashboard from './routes/Dashboard.tsx';
+import ProtectedRoute from './routes/ProtectedRoute.tsx';
+import { AuthProvider } from './context/AuthProvider.tsx';
+import { MantineProvider, createTheme, rem } from '@mantine/core';
+
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  // change theme settings here
+})
+
 
 function App() {
-  return <>
-    <MainNavbar/>
-    <Testing/>
-  </>
+  return (
+    <MantineProvider theme={theme} defaultColorScheme='light'>
+    <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Root />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* All protected routes should be nested here */}
+        {/* Wrap in AppShell */}
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        {/* Error page route */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </AuthProvider>
+    </BrowserRouter>
+    </MantineProvider>
+  )
 }
 
 export default App
