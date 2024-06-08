afe6d9e3b60707378ead2eb5fd2c91b83dc7b5ae
Christopher Tan <phertan02@gmail.com>
2024-06-08 22:43:01 +0800

    move routing to app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,36 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './routes/Root.tsx'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ErrorPage from './components/ErrorPage.tsx'
-import Register from './routes/Register.tsx'
-import Login from './routes/Login.tsx'
-import Dashboard from './routes/Dashboard.tsx';
-import ProtectedRoute from './routes/ProtectedRoute.tsx';
-import { AuthProvider } from './context/AuthProvider.tsx';
+import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Root />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-
-        {/* All protected routes should be nested here */}
-        <Route element={<ProtectedRoute/>}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        {/* Error page route */}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
