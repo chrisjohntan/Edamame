c79a861c0c888edced114087d7f303219b2beba1
Christopher Tan <phertan02@gmail.com>
2024-06-08 21:36:08 +0800

    remove testing component

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeNavbar from '../components/HomeNavbar';
 import Testing from '../components/Testing';
+import Home from '../components/Home';
 
 function Root() {
   return <>
-    <HomeNavbar/>
-    <Testing/>
+    <Home/>
   </>
 }
 
