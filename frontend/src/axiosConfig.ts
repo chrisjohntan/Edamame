153e3fa4c8aea3118e7a0280b1292bd868afaca7
Christopher Tan <phertan02@gmail.com>
2024-06-18 18:21:17 +0800

    default header include xcsrf token

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts: string[] | undefined = value.split(`; ${name}=`);
+  console.log(parts)
+  if (parts.length === 2) return parts?.pop()?.split(';').shift();
+}
+
 // TODO: change to env variable
 const instance = axios.create({baseURL:import.meta.env.VITE_BACKEND_URL})
 instance.defaults.withCredentials=true;
+instance.defaults.headers.common["X-CSRF-TOKEN"] = getCookie("csrf_access_token");
 
 // axios.defaults.baseURL = "http://localhost:8080"
 
