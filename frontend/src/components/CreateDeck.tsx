17c6e048fc9cef4e768565d504c4b5d55709aef1
Christopher Tan <phertan02@gmail.com>
2024-06-24 02:19:41 +0800

    apply validation, change button and title

--- frontend/src/components/CreateDeck.tsx
+++ frontend/src/components/CreateDeck.tsx
@@ -1,14 +1,14 @@
 import { Button, Modal, ModalContent, Paper, TextInput, UnstyledButton, rem } from "@mantine/core";
 import { useState } from "react";
-import { IconPlus, IconX } from "@tabler/icons-react";
+import { IconCircleXFilled, IconPlus, IconX } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
 import axios from "../axiosConfig";
-import { AxiosResponse } from "axios";
+import { AxiosResponse, isAxiosError } from "axios";
 import { Deck } from "../types";
 import { useNavigate } from "react-router-dom";
 
-function CreateDeck() {
+function CreateDeck(props: {addData: (d: Deck)=>void}) {
   const [opened, { open, close }] = useDisclosure();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -16,17 +16,28 @@ function CreateDeck() {
     mode: "controlled",
     initialValues: {
       deckName: ""
+    },
+    validate: {
+      deckName: (value) => value.length <= 0 ? "Cannot be empty" : null
     }
   })
 
-  const handleSubmit = async (data: { deckName: string }) => {
+  const handleCreate = async (data: { deckName: string }) => {
     try {
       setLoading(true);
       const response: AxiosResponse = await axios.post("/create_deck", { deck_name: data.deckName })
+      const deck = response.data.deck as Deck
+      props.addData(deck)
+      form.reset()
       // should navigate to the view deck page to add cards
       // and set timeout
+      // navigate()
     } catch (err) {
-      
+      if (isAxiosError(err)) {
+        form.setErrors({deckName: err.response?.data.message})
+      } else {
+        console.error(err)
+      }
     } finally {
       setLoading(false);
     }
@@ -43,13 +54,14 @@ function CreateDeck() {
         opened={opened}
         onClose={close}
         transitionProps={{ transition: "pop", duration: 0 }}
-        title="Create Deck"
+        title={<><h3>Create Deck</h3></>}
         closeOnClickOutside={false}
         closeOnEscape={false}
         withCloseButton={!loading}>
-        <form onSubmit={form.onSubmit(handleSubmit)}>
+        <form onSubmit={form.onSubmit(handleCreate)}>
           <TextInput label="Deck name" mb="md" autoComplete="off" key={form.key("deckName")}
-            {...form.getInputProps('deckName')} required withAsterisk rightSection={<UnstyledButton onClick={form.reset} component={IconX} stroke={1.5}/>}/>
+            {...form.getInputProps('deckName')} required withAsterisk 
+            rightSection={<UnstyledButton onClick={form.reset} component={IconCircleXFilled}/>}/>
           <Button type="submit" loading={loading}>Create</Button>
         </form>
       </Modal>
