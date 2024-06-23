a82a43810afb05ab3165ead1b907b34101e033f7
Christopher Tan <phertan02@gmail.com>
2024-06-24 02:21:16 +0800

    move data getting here, and view and loading states also

--- frontend/src/components/MultiDeckView.tsx
+++ frontend/src/components/MultiDeckView.tsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Deck } from "../types";
 import SearchBar from "./SearchBar";
 import DeckTable from "./DeckTable";
 import CreateDeck from "./CreateDeck";
+import axios from "../axiosConfig"
 import { Container, Group } from "@mantine/core";
 
 function MultiDeckView() {
   const [searchFilter, setSearchFilter] = useState(""); 
+  const [data, setData] = useState<Deck[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [view, setView] = useState<"table"|"grid">("table")
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get("/get_decks")
+        // Parse response
+        const parsedResponse = await response.data.map((resObj:any): Deck => ({id: resObj.id, deck_name: resObj.deck_name, size: resObj.size}))
+        setData(parsedResponse)
+        console.log(data)
+      } catch (err) {
+        console.error(err);
+      } finally{
+        setLoading(false);
+      }
+    }
+    getData()
+  }, [])
+
+  const addData = (newDeck: Deck) => {
+    setData([...data, newDeck]);
+  }
 
   return (
     <>
@@ -14,9 +40,14 @@ function MultiDeckView() {
         <SearchBar
           searchFilter={searchFilter} 
           onSearchFilterChange={setSearchFilter}/>
-        <CreateDeck/>
+        <CreateDeck addData={addData}/>
       </Group>
-      <DeckTable searchFilter={searchFilter} view="table"/>
+      <DeckTable 
+        searchFilter={searchFilter} 
+        view={view} 
+        data={data} 
+        setData={setData} 
+        loading={loading}/>
     </>
   )
 }
