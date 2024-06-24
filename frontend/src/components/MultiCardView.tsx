4ed612e23bdde1f93e9dfd143b8e1218fbfb3420
Christopher Tan <phertan02@gmail.com>
2024-06-25 01:38:10 +0800

    get data

--- frontend/src/components/MultiCardView.tsx
+++ frontend/src/components/MultiCardView.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import { useEffect, useState } from "react";
 import { Card } from "../types";
+import axios from "../axiosConfig"
+import { AxiosResponse, isAxiosError } from "axios";
+import CardTable from "./CardTable";
 
 
 function MultiCardView() {
@@ -10,20 +13,48 @@ function MultiCardView() {
   const deckId = params.deckId;
   const [searchFilter, setSearchFilter] = useState("");
   const [data, setData] = useState<Card[]>([])
+  const [pageLoading, setPageLoading] = useState(true);
+  const [error, setError] = useState<string|null>("")
+  const [view, setView] = useState<"grid"|"table">("table")
 
   useEffect(() => {
-    const getCards = async () => {
-      
+    const getData = async () => {
+      try {
+        const response = await axios.get("/get_decks")
+        // Parse response
+        // const parsedResponse = response.data.map((resObj:any): Card => ({id: resObj.id, header: resObj.header, size: resObj.size}))
+        setData(response.data)
+        console.log(data)
+      } catch (err) {
+        if (isAxiosError(err)) {
+          setError(err.response?.data.message) 
+        }
+        console.error(err);
+      } finally{
+        setPageLoading(false);
+      }
     }
-  })
+    getData()
+  }, [])
 
-  return (
-    <>
-      <Group>
-        <SearchBar
-          searchFilter={searchFilter}
-          onSearchFilterChange={setSearchFilter}/>
-      </Group>
-    </>
-  )
+  const addData = (newCard: Card) => {
+    setData([...data, newCard])
+  }
+
+  if (error) {
+    return <div>{error}</div>
+  } else {
+    return (
+      <>
+        <Group>
+          <SearchBar
+            searchFilter={searchFilter}
+            onSearchFilterChange={setSearchFilter}/>
+          <CardTable data={data} searchFilter={searchFilter} view={view} />
+        </Group>
+      </>
+    )
+  }
 }
+
+export default MultiCardView;
