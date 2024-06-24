60e4bb3dac311794a25bdcb649751ad60708e429
Christopher Tan <phertan02@gmail.com>
2024-06-25 01:39:23 +0800

    fix sidebar filling whole view

--- frontend/src/routes/CustomAppShell.tsx
+++ frontend/src/routes/CustomAppShell.tsx
@@ -2,13 +2,17 @@ import { AppShell, rem } from "@mantine/core";
 import ProtectedHeader from "../components/ProtectedHeader";
 import { Outlet } from "react-router-dom";
 import ProtectedSidebar from "../components/ProtectedSidebar";
+import { useDisclosure } from "@mantine/hooks";
 
 function CustomAppShell() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <AppShell header={{ height: 60 }}
       navbar={{
         width: rem(200),
         breakpoint: 'sm',
+        collapsed: { mobile: !opened }
       }}
       padding="md">
       <ProtectedHeader />
