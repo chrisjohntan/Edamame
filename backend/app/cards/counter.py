8d043fe7e93c371814851d0b5fbeb83f0cd0500a
Christopher Tan <phertan02@gmail.com>
2024-06-27 23:24:28 +0800

    retrieve counts for a period

--- backend/app/cards/counter.py
+++ backend/app/cards/counter.py
@@ -2,6 +2,7 @@ from ..extensions import db
 from ..models import ReviewCount
 import datetime as dt
 from sqlalchemy import and_
+from typing import List
 
 def addDailyCount(user_id: int):
     today = dt.date.today()
@@ -24,3 +25,16 @@ def addDailyCount(user_id: int):
     db.session.commit()
     
     return 
+
+def getReviewCounts(user_id: int, start_date: dt.date, end_date: dt.date):
+    records: List[ReviewCount] = db.session.query(
+        db.select(ReviewCount)
+        .where(
+            and_(
+                ReviewCount.user_id == user_id,
+                ReviewCount.date >= start_date,
+                ReviewCount.date <= end_date
+            )
+        )
+    ).all()
+    return records
