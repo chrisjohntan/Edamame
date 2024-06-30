19c128c503ac61ce2623ae41efff95e032893c70
Christopher Tan <phertan02@gmail.com>
2024-06-30 18:13:07 +0800

    default content type

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -11,6 +11,7 @@ export function getCookie(name: string) {
 const instance = axios.create({baseURL:import.meta.env.VITE_BACKEND_URL})
 instance.defaults.withCredentials=true;
 // instance.defaults.headers.common["X-CSRF-TOKEN"] = getCookie("csrf_access_token");
+instance.defaults.headers.common["Content-Type"] = "application/json"
 
 instance.interceptors.request.use(
   (config) => {
