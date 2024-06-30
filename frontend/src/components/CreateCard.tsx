45113cb27090b25da57b5f24071310f01a391345
Christopher Tan <phertan02@gmail.com>
2024-06-30 16:44:50 +0800

    manual/auto options

--- frontend/src/components/CreateCard.tsx
+++ frontend/src/components/CreateCard.tsx
@@ -5,13 +5,13 @@ import { useForm } from "@mantine/form";
 import axios from "../axiosConfig";
 import { isAxiosError } from "axios";
 import { IconPlus } from "@tabler/icons-react";
-import { Box, Button, Divider, Flex, Group, Modal, Stack, Text, TextInput, rem } from "@mantine/core";
+import { Box, Button, Divider, Flex, Group, Modal, SegmentedControl, Slider, Stack, Switch, Text, TextInput, Tooltip, rem } from "@mantine/core";
 import { dataToCard } from "./utils";
 
-function CreateCard(props: { addData: (c: Card)=>void, deckId: string }) {
+function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
   const [opened, { open, close }] = useDisclosure();
   const [loading, setLoading] = useState(false);
-  const [type, setType] = useState<"manual"|"auto">("manual")
+  const [type, setType] = useState("manual")
   const form = useForm({
     mode: "controlled",
     initialValues: {
@@ -25,17 +25,20 @@ function CreateCard(props: { addData: (c: Card)=>void, deckId: string }) {
   const handleCreate = async (cardData: any) => {
     try {
       setLoading(true);
-      const response = await axios.post(`/create_card/${props.deckId}`, {...cardData, card_type: type})
+      const response = await axios.post(`/create_card/${props.deckId}`, { ...cardData, card_type: type })
       const card = dataToCard(response.data.card)
       props.addData(card)
     } catch (err) {
       if (isAxiosError(err)) {
-        
+
       }
     } finally {
       setLoading(false);
+      form.reset()
     }
   }
+
+  const typeTooltip = "Auto (WIP): Translates fields automatically"
 
   return (
     <>
@@ -54,26 +57,32 @@ function CreateCard(props: { addData: (c: Card)=>void, deckId: string }) {
         withCloseButton={!loading}
         padding="lg">
         <form onSubmit={form.onSubmit(handleCreate)}>
-          
-          <Group 
+          <Tooltip label={typeTooltip}>
+            <SegmentedControl
+              data={[{ label: "Manual", value: "manual" }, { label: "Auto", value: "auto" }]}
+              value={type}
+              onChange={setType}
+              mb="md" />
+          </Tooltip>
+          <Group
             wrap="nowrap"
             align="start"
-            >
+          >
             <Stack >
               <Text>Front</Text>
               <TextInput label="Header" autoComplete="off" key={form.key("header")}
-              {...form.getInputProps('header')} required withAsterisk />
+                {...form.getInputProps('header')} required withAsterisk />
               <TextInput label="Body" autoComplete="off" key={form.key("body")}
-              {...form.getInputProps('body')} />
-              
+                {...form.getInputProps('body')} />
+
             </Stack>
-            <Divider orientation="vertical" size="md"/>
+            <Divider orientation="vertical" size="md" />
             <Stack >
               <Text>Back</Text>
               <TextInput label="Header" autoComplete="off" key={form.key("header_flipped")}
-              {...form.getInputProps('header_flipped')} required withAsterisk />
+                {...form.getInputProps('header_flipped')} required withAsterisk />
               <TextInput label="Body" autoComplete="off" key={form.key("body_flipped")}
-              {...form.getInputProps('body_flipped')} />
+                {...form.getInputProps('body_flipped')} />
             </Stack>
           </Group>
           <Button type="submit" w="100%" mt="lg">Add</Button>
