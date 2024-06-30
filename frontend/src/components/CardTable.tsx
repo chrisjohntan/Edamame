ad3dfa30921160dedb93e7a285a92b4c21ad70f4
Christopher Tan <phertan02@gmail.com>
2024-06-30 16:43:43 +0800

    card dropdown options

--- frontend/src/components/CardTable.tsx
+++ frontend/src/components/CardTable.tsx
@@ -1,9 +1,12 @@
-import { Text, Table, UnstyledButton, Group, Center, rem, ScrollArea, keys, LoadingOverlay, ActionIcon, Box, Flex, Card as MantineCard, Menu, Grid, SimpleGrid, Button, Divider } from "@mantine/core";
+import { Text, Table, UnstyledButton, Group, Center, rem, ScrollArea, keys, LoadingOverlay, ActionIcon, Box, Flex, Card as MantineCard, Menu, Grid, SimpleGrid, Button, Divider, Title } from "@mantine/core";
 import { IconChevronUp, IconChevronDown, IconSelector, IconPencil, IconTrash, IconDotsVertical } from "@tabler/icons-react";
 import classes from './styles/Table.module.css';
 import { useEffect, useState } from "react";
 import type { Card, Deck } from "../types";
 import axios from "../axiosConfig"
+import DeleteCard from "./DeleteCard";
+import EditCard from "./EditCard";
+import CardViewer from "./CardViewer";
 
 interface ThProps {
   children: React.ReactNode;
@@ -30,8 +33,8 @@ function Th({ children, descending, sorted, onSort }: ThProps) {
   );
 }
 
-function TdText({children}: {children: React.ReactNode}) {
-  return ( 
+function TdText({ children }: { children: React.ReactNode }) {
+  return (
     <Table.Td>
       <Text truncate="end" fz="sm">
         {children}
@@ -47,7 +50,7 @@ function filterData(data: Card[], filter: string) {
   }
   const query = filter.toLowerCase().trim();
   const fieldExclude = [
-    
+
   ]
   // 
   // return data.filter(item => item?.header.includes(query));
@@ -58,43 +61,7 @@ function filterData(data: Card[], filter: string) {
   );
 }
 
-// display the Card data in mantine card
-const DisplayCard = ({ cardData } : {cardData: Card}) => {
 
-  return (
-    <MantineCard shadow="sm" padding="lg" radius="md" withBorder>
-      
-      {/* <MantineCard.Section>
-
-      </MantineCard.Section> */}
-      <Group justify="space-between" >
-        <Text truncate style={{flexGrow:1}}>{cardData.header}</Text>
-        <Menu>
-          <Menu.Target>
-            <ActionIcon variant="transparent" >
-              <IconDotsVertical/>
-            </ActionIcon>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Label>Actions</Menu.Label>
-            <Menu.Item leftSection={<IconPencil style={{ width: rem(14), height: rem(14) }} />}>
-              Edit card
-            </Menu.Item>
-            <Menu.Item 
-              leftSection={<IconTrash style={{ width: rem(14), height: rem(14) }} />} 
-              >
-              Delete
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </Group>
-      <Text mt="md">
-        {cardData.header_flipped}
-      </Text>
-
-    </MantineCard>
-  )
-}
 
 function sortData(
   data: Card[],
@@ -132,28 +99,55 @@ function sortData(
 
 
 function CardTable(props: {
-  searchFilter: string, 
-  view: "grid"|"table", 
+  searchFilter: string,
+  view: "grid" | "table",
   data: Card[],
-  setData: (c:Card[])=>void,
+  setData: (c: Card[]) => void,
   loading: boolean
 }) {
-  const [sortBy, setSortBy] = useState<keyof Omit<Card,"id"> | null>(null)
+  const [sortBy, setSortBy] = useState<keyof Omit<Card, "id"> | null>(null)
   const [descending, setDescending] = useState(false);
   const [sortedData, setSortedData] = useState<Card[]>(props.data);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setSortedData(sortData(props.data, {sortBy, descending: descending, search: props.searchFilter}))
+    setSortedData(sortData(props.data, { sortBy, descending: descending, search: props.searchFilter }))
     console.log("sorting")
     return
   }, [props.searchFilter, props.data])
-  
+
   function handleSort(field: keyof Omit<Card, "id">) {
     const desc = field === sortBy ? !descending : false
     setSortBy(field);
     setDescending(desc);
-    setSortedData(sortData(props.data, {sortBy, descending: descending, search: props.searchFilter}))
+    setSortedData(sortData(props.data, { sortBy, descending: descending, search: props.searchFilter }))
+  }
+
+  // display the Card data in mantine card
+  const DisplayCard = ({ cardData }: { cardData: Card }) => {
+    return (
+      <MantineCard shadow="sm" padding="lg" radius="md" withBorder onClick={() => console.log("a")}>
+        <Group justify="space-between" >
+          <Title lineClamp={1} order={4} style={{ flexGrow: 1 }} >{cardData.header}</Title>
+          <Box onClick={e => e.stopPropagation()}>
+            <Menu shadow="lg" keepMounted>
+              <Menu.Target>
+                <ActionIcon variant="transparent">
+                  <IconDotsVertical />
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Label>Actions</Menu.Label>
+                <EditCard card={cardData} data={props.data} setData={props.setData}/>
+                <DeleteCard card={cardData} data={sortedData} setData={setSortedData} />
+              </Menu.Dropdown>
+            </Menu>
+          </Box>
+        </Group>
+        <Text mt="md" lineClamp={4}>
+          {cardData.body}
+        </Text>
+      </MantineCard>
+    )
   }
 
   if (props.view === "table") {
@@ -174,10 +168,9 @@ function CardTable(props: {
         </Table.Td>
       </Table.Tr>
     ));
-
     return (
       <ScrollArea>
-        <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 3 }} />
+        <LoadingOverlay visible={props.loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 3 }} />
         <Table horizontalSpacing="md" verticalSpacing="xs" miw={700} layout="fixed" withTableBorder>
           <Table.Thead>
             <Table.Tr>
@@ -216,7 +209,7 @@ function CardTable(props: {
               >
                 Reviews Done
               </Th>
-              <Table.Th className={classes.empty}/>
+              <Table.Th className={classes.empty} />
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
@@ -236,29 +229,29 @@ function CardTable(props: {
         </Table>
       </ScrollArea>
     )
-  } 
+  }
 
   if (props.view == "grid") {
     const rows = sortedData?.map(card => (
-      <DisplayCard cardData={card} key={card.id}/>
+      <DisplayCard cardData={card} key={card.id} />
     ))
 
     return (
       rows.length <= 0 ? (
-      <Text fw={500} ta="center">
-        Nothing found
-      </Text> 
-      ) : 
-      <ScrollArea >
-        <SimpleGrid cols={{ base: 1, xs: 2, md: 3, lg: 4, xl: 5 }}>
-          {rows}
-        </SimpleGrid>
-        <Divider mt="lg" label="You have reached the end" />
-      </ScrollArea>
+        <Text fw={500} ta="center">
+          Nothing found
+        </Text>
+      ) :
+        <ScrollArea >
+          <SimpleGrid cols={{ base: 1, xs: 2, md: 3, lg: 4, xl: 5,  }}>
+            {rows}
+          </SimpleGrid>
+          <Divider mt="lg" label="You have reached the end" />
+        </ScrollArea>
     )
   }
 
-  if (loading) {
+  if (props.loading) {
     return <LoadingOverlay visible={props.loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 3 }} />
   }
 }
