0427483b44bc69027e075fc4368593a635c619ee
Christopher Tan <phertan02@gmail.com>
2024-07-02 00:35:54 +0800

    review card to backend

--- frontend/src/components/CardViewer.tsx
+++ frontend/src/components/CardViewer.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Group, LoadingOverlay, Modal, Text } from "@mantine/core";
+import { Box, Button, Center, Divider, Flex, Group, LoadingOverlay, Modal, Text } from "@mantine/core";
 import { Card } from "../types";
 import { useEffect, useState } from "react";
 import axios from "../axiosConfig";
 import { isAxiosError } from "axios";
 import { dataToCard } from "./utils";
+import { notifications } from "@mantine/notifications";
 
 // if Card is passed as prop, render the card
 // else call backend for next card in the deck
@@ -15,6 +16,7 @@ function CardViewer(props: { card?: Card, deckId: number, opened: boolean, onClo
    * @todo: should put this in parent
    */
   const [currentCard, setCurrentCard] = useState<Card>()
+  
 
   const [side, setSide] = useState<"front" | "back">("front");
   const toggleSide = () => {
@@ -26,16 +28,18 @@ function CardViewer(props: { card?: Card, deckId: number, opened: boolean, onClo
   }
 
   const getNextCard = async () => {
+    setSide("front")
     try {
       console.log("getting next card..")
       setLoading(true);
-      const response = await axios.put(`/next_card/${props.deckId}`, {});
+      const response = await axios.put(`/next_card/${props.deckId}`, {ignore_review_time: true});
       console.log(response)
       setCurrentCard(dataToCard(response.data.card));
     } catch (err) {
       if (isAxiosError(err)) {
         if (err.response?.status === 404) {
           console.log("no cards found")
+          notifications.show({color: "red", message: err.response.data.message, withBorder: true})
         }
       }
       props.onClose();
@@ -54,13 +58,18 @@ function CardViewer(props: { card?: Card, deckId: number, opened: boolean, onClo
     }
   }, [props.opened])
 
-  const handleNext = async (ease: number) => {
-    if (currentCard) {
-      try {
-        const reponse = axios.put(`/review_card/${currentCard.id}/${ease}`);
-        
-      } catch (err) {
-
+  const handleNext = (ease: number) => {
+    return async () => {
+      if (currentCard) {
+        try {
+          const response = await axios.put(`/review_card/${currentCard.id}/${ease}`, {});
+          notifications.show({message: response.data.message , withBorder:true})
+          getNextCard();
+        } catch (err) {
+          if (isAxiosError(err)) {
+            
+          }
+        }
       }
     }
   }
@@ -72,31 +81,33 @@ function CardViewer(props: { card?: Card, deckId: number, opened: boolean, onClo
         <Modal
           // opened={props.opened}
           opened={props.opened}
-          onClose={props.onClose}
+          onClose={()=>{props.onClose();setSide("front")}}
         >
           <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
           {
             side === "front"
               ? (
                 <Box>
-                  <Text ta="center" mb="md">{currentCard.header}</Text>
+                  <Text ta="center" mb="md" size="lg">{currentCard.header}</Text>
+                  <Divider size="sm" mb="sm" />
                   <Text ta="center" mb="md">{currentCard.body}</Text>
-                  <Button onClick={toggleSide}>Show back</Button>
+                  <Center><Button onClick={toggleSide}>Show back</Button></Center>
                 </Box>
               ) : (
                 <Box>
-                  <Text ta="center" mb="md">{currentCard.header_flipped}</Text>
+                  <Text ta="center" mb="md" size="lg">{currentCard.header_flipped}</Text>
+                  <Divider size="sm" mb="sm" />
                   <Text ta="center" mb="md">{currentCard.body_flipped}</Text>
-                  <Button onClick={toggleSide} mb="xl">Show front</Button>
+                  <Center><Button onClick={toggleSide} mb="xl">Show front</Button></Center>
                   {/* <Button onClick={()=>{toggleSide();getNextCard();}}>Next card</Button> */}
-                  <Button>
-                    <Button.Group variant="default" >
-                      <Button variant="default">Forgot</Button>
-                      <Button variant="default">Hard</Button>
-                      <Button variant="default">Okay</Button>
-                      <Button variant="default">Easy</Button>
+                  <Flex justify="center">
+                    <Button.Group variant="default">
+                      <Button variant="outline" color="black" onClick={handleNext(1)}>Forgot</Button>
+                      <Button variant="outline" color="red" onClick={handleNext(2)}>Hard</Button>
+                      <Button variant="outline" color="green" onClick={handleNext(3)}>Okay</Button>
+                      <Button variant="outline" color="blue" onClick={handleNext(4)}>Easy</Button>
                     </Button.Group>
-                  </Button>
+                  </Flex>
                 </Box>
               )
           }
