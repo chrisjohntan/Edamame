bc4a33ec2a3dd0264b93e2a9c05fead69f8b60e5
Christopher Tan <phertan02@gmail.com>
2024-07-02 00:12:55 +0800

    clean up

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -10,9 +10,7 @@ export function getCookie(name: string) {
 // TODO: change to env variable
 const instance = axios.create({baseURL:import.meta.env.VITE_BACKEND_URL})
 instance.defaults.withCredentials=true;
-// instance.defaults.headers.common["X-CSRF-TOKEN"] = getCookie("csrf_access_token");
 instance.defaults.headers.common["Content-Type"] = "application/json"
-
 instance.interceptors.request.use(
   (config) => {
     const csrfToken = getCookie("csrf_access_token");
@@ -26,6 +24,5 @@ instance.interceptors.request.use(
   }
 );
 
-// axios.defaults.baseURL = "http://localhost:8080"
 
 export default instance;
