b9018b46cae75e17757a5309d6f1f604b4c4290a
Christopher Tan <phertan02@gmail.com>
2024-07-02 00:55:26 +0800

    close modal

--- frontend/src/components/EditCard.tsx
+++ frontend/src/components/EditCard.tsx
@@ -27,9 +27,11 @@ function EditCard(
       setLoading(true);
       const response = await axios.put(`/edit_card/${card.id}`, cardData)
       const returnCard = dataToCard(response.data.card)
-      
+      close();
     } catch (err) {
 
+    } finally {
+      setLoading(false);
     }
   }
 
