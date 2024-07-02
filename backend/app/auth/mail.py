376313c438a19ef76b5ef8d2719c655fe62e5a3a
Jia Hong <jhkoh0416@gmail.com>
2024-07-03 00:25:54 +0800

    defined functions for encode and decode

--- backend/app/auth/mail.py
+++ backend/app/auth/mail.py
@@ -1,16 +1,14 @@
 import smtplib, ssl
 from dotenv import load_dotenv
 import os
+import jwt
 
 load_dotenv()
 
 
 port = 465  # For SSL
-
 sender_email = "edamameorbital@gmail.com"
 password = os.environ.get("EMAIL_PASSWORD")
-
-
 # Create a secure SSL context
 context = ssl.create_default_context()
 
@@ -18,3 +16,9 @@ def send_email(receiver_email: str, message: str):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+
+def encode_token(key, payload):
+    return jwt.encode(payload, key, algorithm="HS256")
+
+def decode_token(key, encoded):
+    return jwt.decode(encoded, key, algorithms="HS256")
