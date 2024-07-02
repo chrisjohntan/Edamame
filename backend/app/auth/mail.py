4b8f190b9b1c4710edbffdee0e10445546ef2f56
Jia Hong <jhkoh0416@gmail.com>
2024-07-02 23:22:40 +0800

    sending email

---
+++ backend/app/auth/mail.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+port = 465  # For SSL
+
+sender_email = "edamameorbital@gmail.com"
+password = os.environ.get("EMAIL_PASSWORD")
+
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+def send_email(receiver_email: str, message: str):
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+send_email("edamameorbital@gmail.com", "hi")
