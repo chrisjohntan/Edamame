19c55a661d1cc600834c7a3e66280b6d05a1fca3
Christopher Tan <phertan02@gmail.com>
2024-07-04 22:35:58 +0800

    add year selector

--- frontend/src/components/Heatmap.tsx
+++ frontend/src/components/Heatmap.tsx
@@ -2,66 +2,100 @@ import { useEffect, useState } from "react";
 import axios from "../axiosConfig"
 import HeatMap from "@uiw/react-heat-map"
 import { dateToIso } from "./utils";
-import { Tooltip, rem } from "@mantine/core";
+import { Box, LoadingOverlay, NumberInput, Paper, Stack, Tooltip, rem } from "@mantine/core";
 import { format } from "date-fns"
 
 const mapper = () => {
   
 }
 
+type HeatMapData = {
+  date: string,
+  count: number
+}
+
 
 function Heatmap() {
-
-  const [data, setData] = useState<{date: string, count: number}[]>([])
+  const currentYear = new Date().getFullYear();
+  const [data, setData] = useState<HeatMapData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [year, setYear] = useState(currentYear);
+  const changeYear = (year: number | string) => {
+    year = Number(year);
+    if (year <= currentYear) {
+      setYear(year);
+    } else {
+      setYear(currentYear);
+    }
+  }
 
   useEffect(()=> {
     const getReviewHistory = async () => {
+      setLoading(true);
       try {
         const now = new Date()
         // const today = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`
         const today = dateToIso(now);
-        const start = `${now.getFullYear()}-01-01`
-        console.log(today)
-        const response = await axios.get(`/get_review_counts?start_date=${start}&end_date=${today}`)
+        const start = `${now.getFullYear()}-01-01`;
+        console.log(today);
+        const response = await axios.get(`/get_review_counts?start_date=${start}&end_date=${today}`);
 
         console.log(response);
+        const raw: {date: string, review_count: number, user_id: number}[] = response.data.review_counts;
+        const parsed: HeatMapData[] = raw.map(obj => ({date: obj.date, count: obj.review_count}));
+        setData(parsed);
+        console.log(data)
       } catch (err) {
-
+        console.error(err);
+      } finally {
+        setLoading(false);
       }
     }
     getReviewHistory();
-  })
-
-  
+  }, [year])
 
   return (
-    <HeatMap 
-      style={{border: "black"}}
-      value={[{date: "2024/02/02", count: 2}, ]}
-      width={850}
-      height={300}
-      rectSize={15}
-
-      endDate={new Date('2024/12/31')}
-      startDate={new Date('2023/12/31')}
-      rectRender={(props, data) => {
-        const msg = data.count
-          ? data.count === 1
-            ? "1 review"
-            : `${data.count} reviews`
-          : "No reviews"
-        const date = new Date(data.date)
-        return (
-          <Tooltip 
-            arrowOffset={50} 
-            arrowSize={8}
-            withArrow
-            label={`${msg} on ${format(date, "MMMM do")}`}>
-            <rect {...props} />
-          </Tooltip>
-        )
-      }}
+    <>
+      <NumberInput 
+        value={year} 
+        onChange={changeYear}
+        max={currentYear}
+        min={currentYear-3}
+        allowDecimal={false}
+        style={{width: "5rem"}}
+        size="sm"
+        mb="md"
       />
+      {/* <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} style={{borderWidth:"10px"}}/> */}
+      <HeatMap
+        style={{backgroundColor:""}}
+        value={data}
+        width={950}
+        height={170}
+        rectSize={15}
+        endDate={new Date(`${year}/12/31`)}
+        startDate={new Date(`${year - 1}/12/31`)}
+        
+        rectRender={(props, data) => {
+          const msg = data.count
+            ? data.count === 1
+              ? "1 review"
+              : `${data.count} reviews`
+            : "No reviews"
+          const date = new Date(data.date)
+          return (
+            <Tooltip
+              arrowOffset={50}
+              arrowSize={8}
+              withArrow
+              label={`${msg} on ${format(date, "MMMM do")}`}>
+              <rect {...props} />
+            </Tooltip>
+          )
+        }}
+        
+        />
+    </>
   )
 }
 
