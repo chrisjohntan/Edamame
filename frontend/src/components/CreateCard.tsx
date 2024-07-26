8e308d995b495ceb42aca177f580a2f097505b25
Christopher Tan <phertan02@gmail.com>
2024-07-26 22:04:16 +0800

    get translations before confirming card

--- frontend/src/components/CreateCard.tsx
+++ frontend/src/components/CreateCard.tsx
@@ -7,11 +7,13 @@ import { isAxiosError } from "axios";
 import { IconPlus } from "@tabler/icons-react";
 import { Box, Button, Divider, Flex, Group, Modal, SegmentedControl, Slider, Stack, Switch, Text, TextInput, Tooltip, rem } from "@mantine/core";
 import { dataToCard } from "./utils";
+import { notifications } from "@mantine/notifications";
 
 function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
   const [opened, { open, close }] = useDisclosure();
   const [loading, setLoading] = useState(false);
-  const [type, setType] = useState("manual")
+  const [type, setType] = useState("manual");
+  const [translated, setTranslated] = useState(false);
   const form = useForm({
     mode: "controlled",
     initialValues: {
@@ -22,22 +24,60 @@ function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
     }
   })
 
-  const handleCreate = async (cardData: any) => {
+  const getTempCard = async (data: {header: string, body?: string}) => {
     try {
       setLoading(true);
-      console.log(cardData)
-      const response = await axios.post(`/create_card/${props.deckId}`, { ...cardData, card_type: type })
-      const card = dataToCard(response.data.card)
-      props.addData(card)
-      close()
+      const payload = {
+        header: data.header,
+        body: data.body
+      }
+      console.log(payload)
+      const response = await axios.post(`/get_translation_for_card`, payload)
+      const tempData: {header:string, body:string, header_flipped:string} = response.data
+      form.setFieldValue("header", tempData.header)
+      form.setFieldValue("body", tempData.body)
+      form.setFieldValue("header_flipped", tempData.header_flipped)
     } catch (err) {
+      console.error(err)
       if (isAxiosError(err)) {
-
+        notifications.show({
+          message: err.response?.data.message,
+          color: "red"
+        })
       }
     } finally {
+      setTranslated(true);
       setLoading(false);
-      form.reset()
     }
+  }
+
+  const handleCreate = async (cardData: any) => {
+    if (type === "auto" && !translated) {
+      getTempCard(cardData)
+    } else {
+      try {
+        setLoading(true);
+        console.log(cardData)
+        const response = await axios.post(`/create_card/${props.deckId}`, { ...cardData, card_type: type })
+        const card = dataToCard(response.data.card)
+        props.addData(card)
+        onClose()
+      } catch (err) {
+        if (isAxiosError(err)) {
+          
+        }
+      } finally {
+        setLoading(false);
+        form.reset()
+      }
+    }
+    setTranslated(false)
+  }
+
+  const onClose = () => {
+    close();
+    form.reset()
+    setTranslated(false);
   }
 
   const typeTooltipText = "Auto (WIP): Translates fields automatically"
@@ -52,7 +92,7 @@ function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
       </Button>
       <Modal size="md"
         opened={opened}
-        onClose={close}
+        onClose={onClose}
         title={<Text size="xl">New card</Text>}
         closeOnClickOutside={false}
         closeOnEscape
@@ -64,6 +104,7 @@ function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
               data={[{ label: "Manual", value: "manual" }, { label: "Auto", value: "auto" }]}
               value={type}
               onChange={setType}
+              disabled={loading}
               mb="md"
             />
           </Tooltip>
@@ -75,17 +116,12 @@ function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
               <Text>Front</Text>
               <TextInput label="Header" autoComplete="off" key={form.key("header")}
                 {...form.getInputProps('header')} required withAsterisk />
-              {
-                type === "manual"
-                  ?
-                  <TextInput label="Body" autoComplete="off" key={form.key("body")}
-                    {...form.getInputProps('body')} />
-                  : null
-              }
-
+              <TextInput label="Body" autoComplete="off" key={form.key("body")}
+                {...form.getInputProps('body')} />  
             </Stack>
+
             {
-              type === "manual"
+              type === "manual" || translated
                 ?
                 <>
                   <Divider orientation="vertical" size="md" />
@@ -101,7 +137,13 @@ function CreateCard(props: { addData: (c: Card) => void, deckId: number }) {
                 null
             }
           </Group>
-          <Button type="submit" w="100%" mt="lg">Add</Button>
+          {
+            type === "auto" && !translated
+              ?
+              <Button type="submit" mt="lg" fullWidth>Translate</Button>
+              :
+              <Button type="submit" mt="lg" fullWidth>Add</Button>
+          }
         </form>
       </Modal>
     </>
