63630d66b7a937a465b0e0e203b0d504f21dd5ed
Christopher Tan <phertan02@gmail.com>
2024-08-14 21:55:44 +0800

    add interceptor for 500 errors

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { notifications } from "@mantine/notifications";
 
 export function getCookie(name: string) {
   const value = `; ${document.cookie}`;
@@ -7,7 +8,7 @@ export function getCookie(name: string) {
   if (parts.length === 2) return parts?.pop()?.split(';').shift();
 }
 
-// TODO: change to env variable
+
 const instance = axios.create({baseURL:import.meta.env.VITE_BACKEND_URL})
 instance.defaults.withCredentials=true;
 instance.defaults.headers.common["Content-Type"] = "application/json"
@@ -20,6 +21,28 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 500 error interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // Return response if successful
+    return response;
+  },
+  (error) => {
+    // Check for 500 errors
+    console.log("intercepted 500")
+    if (error.response && error.response.status === 500) {
+      // Display error notification
+      notifications.show({
+        title: "Server Error",
+        message: "An unexpected error occurred. Please try again later.",
+        color: "red",
+      });
+    }
+    // reject the promise to allow further error handling
     return Promise.reject(error);
   }
 );
